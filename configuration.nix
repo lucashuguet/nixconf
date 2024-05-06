dc2ab523c6cedbf2391e773ca7386059bd7ee4be
AstroGoat <lucashuguet31@gmail.com>
2024-05-06 15:46:59 +0200

    Initial Commit

---
+++ configuration.nix
@@ -0,0 +1,257 @@
+{ config, pkgs, pkgs-unstable, ... }:
+
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+    ];
+
+  boot = {
+    kernelParams = [ "video=1920x1080@60" ];
+    # kernelPackages = pkgs.linuxPackages_latest;
+    initrd.kernelModules = [ "nvidia" ];
+    loader.efi.canTouchEfiVariables = true;
+    extraModprobeConfig = ''
+      blacklist nouveau
+      options snd-hda-intel model=asus-zenbook
+      options nvidia-drm modeset=1
+    '';
+
+    loader.grub = {
+      enable = true;
+      device = "nodev";
+      efiSupport = true;
+      configurationLimit = 10;
+    };
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Paris";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "fr_FR.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "fr_FR.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
+  };
+
+  services.xserver.enable = true;
+
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+    extraPackages = with pkgs; [vaapiVdpau];
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    prime = {
+      offload = {
+	enable = true;
+        enableOffloadCmd =  true;
+      };
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+
+  services.xserver.libinput.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.setupCommands = ''
+    ${pkgs.xorg.xrandr}/bin/xrandr --output eDP-1 --mode 1920x1080 --rate 60
+  '';
+  services.xserver.desktopManager.gnome.enable = true;
+
+  services.xserver.xkb = {
+    layout = "fr";
+    variant = "azerty";
+  };
+  console.keyMap = "fr";
+
+  services.printing.enable = true;
+  services.printing.drivers = with pkgs; [ brlaser ];
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  hardware.bluetooth.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+    wireplumber.enable = true;
+  };
+
+  services.auto-cpufreq.enable = true;
+  services.asusd.enable = true;
+  services.pcscd.enable = true;
+  services.blueman.enable = true;
+  # services.mpd.enable = true;
+
+  services.udev.extraRules = ''
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0660", GROUP="uucp"
+  '';
+
+  users.users.astrogoat = {
+    isNormalUser = true;
+    description = "AstroGoat";
+    extraGroups = [ "networkmanager" "wheel" "audio" "video" "libvirtd" "uucp" "docker" ];
+    shell = pkgs.fish;
+    packages = with pkgs; [];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.variables.XCURSOR_SIZE = "32";
+  environment.variables.EDITOR = "nvim";
+  environment.variables.PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+  # environment.variables.LD_LIBRARY_PATH = lib.mkForce "${pkgs.stdenv.cc.cc.lib}/lib";
+
+  environment.systemPackages = 
+    (with pkgs; [
+      neofetch
+      neovim
+      librewolf
+      mpv
+      thunderbird
+      eza
+      bat
+      fish
+      starship
+      hyprland
+      rofi-wayland
+      waybar
+      python3
+      python311Packages.numpy
+      pavucontrol
+      git
+      pywal
+      imv
+      sxiv
+      mpd
+      ncmpcpp
+      mpc-cli
+      prismlauncher
+      zathura
+      auto-cpufreq
+      jdk17
+      jdk8
+      (pass.withExtensions (ext: with ext ; [pass-otp]))
+      pass
+      gnupg
+      pinentry
+      pinentry-gtk2
+      emacs
+      mangal
+      aria
+      asusctl
+      yt-dlp
+      home-manager
+      swww
+      dunst
+      qutebrowser
+      libnotify
+      wl-clipboard
+      light
+      virt-manager
+      steam
+      lutris
+      ledger-live-desktop
+      libreoffice
+      docker
+      docker-compose
+      p7zip
+      wine
+      qrencode
+      zbar
+      rustup
+      blueman
+      ncdu
+      vimv
+      rust-analyzer
+      gcc
+      nodejs
+      pkg-config
+      openssl
+      espeak
+      ffmpeg
+    ])
+
+    ++
+
+    (with pkgs-unstable; [
+      alacritty
+      nh
+    ]);
+
+  fonts.packages = 
+    (with pkgs-unstable; [
+      (nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    ])
+
+    ++
+
+    (with pkgs; [
+      noto-fonts
+      noto-fonts-emoji
+      noto-fonts-cjk
+    ]);
+
+  fonts.fontconfig.defaultFonts = {
+    serif = [ "Noto Serif" ];
+    sansSerif = [ "Noto Sans" ];
+    monospace = [ "FantasqueSansM Nerd Font" ];
+  };
+
+  programs.hyprland.enable = true;
+  programs.fish.enable = true;
+  programs.light.enable = true;
+
+  # programs.nh = {
+  #   enable = true;
+  #   clean.enable = true;
+  #   clean.extraArgs = "--keep-since 4d --keep 3";
+  #   flake = "/home/astrogoat/nixos";
+  # };
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
+
+  virtualisation.libvirtd.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
+  virtualisation.docker.enable = true;
+
+  programs.virt-manager.enable = true;
+
+  programs.gnupg.agent = {
+    enable = true;
+    pinentryFlavor = "gtk2";
+    enableSSHSupport = true;
+  };
+
+  system.stateVersion = "23.11"; # Did you read the comment?
+}
---
+++ scripts/rofitheme.sh
@@ -0,0 +1,184 @@
+#!/usr/bin/env bash
+
+file=$(find ~/Pictures/wallpapers/ -name '*.png' -exec basename {} \; | sed "s/.png\$//g" | rofi -dmenu)
+
+if [ $? == 1 ]; then	
+  exit 0
+fi
+
+convert_rgba() {
+    color="$1"
+    r=$((16#${color:1:2}))
+    g=$((16#${color:3:2}))
+    b=$((16#${color:5:2}))
+    echo "rgba($r, $g, $b, 100%)"
+}
+
+clamp() {
+    n="$1"
+    smallest="$2"
+    largest="$3"
+    echo $(($n < $smallest ? $smallest : ($n > $largest ? $largest : $n)))
+}
+
+twolen() {
+    hexa="$1"
+    if [ ${#hexa} -eq 1 ]; then
+        echo "0$hexa"
+    else
+        echo "$hexa"
+    fi
+}
+
+light_color() {
+    color="$1"
+    if [ ${#color} -ne 7 ]; then
+        exit 1
+    fi
+    r="${color:1:2}"
+    g="${color:3:2}"
+    b="${color:5:2}"
+
+    rgb=($(printf '%d ' "0x$r" "0x$g" "0x$b"))
+    light=($(clamp $((rgb[0] * 130 / 100)) 0 255) $(clamp $((rgb[1] * 130 / 100)) 0 255) $(clamp $((rgb[2] * 130 / 100)) 0 255))
+
+    echo "#$(twolen $(printf '%x' ${light[0]}))$(twolen $(printf '%x' ${light[1]}))$(twolen $(printf '%x' ${light[2]}))"
+}
+
+wal -qntes -i ~/Pictures/wallpapers/$file.png
+
+source ~/.cache/wal/colors.sh
+
+swww img $wallpaper -t none
+
+black=$color0
+red=$color1
+green=$color2
+yellow=$color3
+blue=$color4
+magenta=$color5
+cyan=$color6
+white=$color7
+
+lblack=$color8
+lred=$(light_color $red)
+lgreen=$(light_color $green)
+lyellow=$(light_color $yellow)
+lblue=$(light_color $blue)
+lmagenta=$(light_color $magenta)
+lcyan=$(light_color $cyan)
+lwhite=$(light_color $white)
+
+lforeground=$white
+lbackground=$color2
+
+colors_path=~/.config/colors
+
+alacritty=$colors_path/alacritty.toml
+qutebrowser=$colors_path/qutebrowser.yml
+rofi=$colors_path/rofi.rasi
+hyprland=$colors_path/hyprland.conf
+waybar=$colors_path/waybar.css
+dunst=$colors_path/dunst.toml
+aura=$colors_path/aura.txt
+
+dunstc=~/.config/dunst/config.toml
+dunstrc=~/.config/dunst/dunstrc
+
+rm $alacritty
+echo "[colors.primary]" | tee -a $alacritty
+echo "background = '$background'" | tee -a $alacritty
+echo "foreground = '$foreground'" | tee -a $alacritty
+echo "[colors.normal]" | tee -a $alacritty
+echo "black = '$black'" | tee -a $alacritty
+echo "red = '$red'" | tee -a $alacritty
+echo "green = '$green'" | tee -a $alacritty
+echo "yellow = '$yellow'" | tee -a $alacritty
+echo "blue = '$blue'" | tee -a $alacritty
+echo "magenta = '$magenta'" | tee -a $alacritty
+echo "cyan = '$cyan'" | tee -a $alacritty
+echo "white = '$white'" | tee -a $alacritty
+echo "[colors.bright]" | tee -a $alacritty
+echo "black = '$lblack'" | tee -a $alacritty
+echo "red = '$lred'" | tee -a $alacritty
+echo "green = '$lgreen'" | tee -a $alacritty
+echo "yellow = '$lyellow'" | tee -a $alacritty
+echo "blue = '$lblue'" | tee -a $alacritty
+echo "magenta = '$lmagenta'" | tee -a $alacritty
+echo "cyan = '$lcyan'" | tee -a $alacritty
+echo "white = '$lwhite'" | tee -a $alacritty
+
+rm $qutebrowser
+echo "colors:" | tee -a $qutebrowser
+echo "  primary:" | tee -a $qutebrowser
+echo "    background: '$background'" | tee -a $qutebrowser
+echo "    foreground: '$foreground'" | tee -a $qutebrowser
+echo "  normal:" | tee -a $qutebrowser
+echo "    black: '$black'" | tee -a $qutebrowser
+echo "    red: '$red'" | tee -a $qutebrowser
+echo "    green: '$green'" | tee -a $qutebrowser
+echo "    yellow: '$yellow'" | tee -a $qutebrowser
+echo "    blue: '$blue'" | tee -a $qutebrowser
+echo "    magenta: '$magenta'" | tee -a $qutebrowser
+echo "    cyan: '$cyan'" | tee -a $qutebrowser
+echo "    white: '$white'" | tee -a $qutebrowser
+echo "  bright:" | tee -a $qutebrowser
+echo "    black: '$lblack'" | tee -a $qutebrowser
+echo "    red: '$lred'" | tee -a $qutebrowser
+echo "    green: '$lgreen'" | tee -a $qutebrowser
+echo "    yellow: '$lyellow'" | tee -a $qutebrowser
+echo "    blue: '$lblue'" | tee -a $qutebrowser
+echo "    magenta: '$lmagenta'" | tee -a $qutebrowser
+echo "    cyan: '$lcyan'" | tee -a $qutebrowser
+echo "    white: '$lwhite'" | tee -a $qutebrowser
+
+rm $rofi
+echo "* {" | tee -a $rofi
+echo "    red: $(convert_rgba $red);" | tee -a $rofi
+echo "    blue: $(convert_rgba $blue);" | tee -a $rofi
+echo "    foreground: $(convert_rgba $foreground);" | tee -a $rofi
+echo "    background: $(convert_rgba $background);" | tee -a $rofi
+echo "    lightfg: $(convert_rgba $lbackground);" | tee -a $rofi
+echo "    lightbg: $(convert_rgba $background);" | tee -a $rofi
+echo "}" | tee -a $rofi
+
+rm $hyprland
+echo "general {" | tee -a $hyprland
+echo "    col.active_border = rgba(${lforeground:1}ee)" | tee -a $hyprland
+echo "    col.inactive_border = rgba(${lbackground:1}ee)" | tee -a $hyprland
+echo "}" | tee -a $hyprland
+echo "decoration {" | tee -a $hyprland
+echo "    col.shadow = rgba(${background:1}ee)" | tee -a $hyprland
+echo "}" | tee -a $hyprland
+
+rm $waybar
+echo "@define-color foreground $foreground;" | tee -a $waybar
+echo "@define-color background $background;" | tee -a $waybar
+echo "@define-color red $red;" | tee -a $waybar
+echo "@define-color green $green;" | tee -a $waybar
+echo "@define-color yellow $yellow;" | tee -a $waybar
+echo "@define-color blue $blue;" | tee -a $waybar
+echo "@define-color magenta $magenta;" | tee -a $waybar
+echo "@define-color cyan $cyan;" | tee -a $waybar
+echo "@define-color white $white;" | tee -a $waybar
+echo "@define-color lblack $lblack;" | tee -a $waybar
+
+rm $dunst
+echo "[global]" | tee -a $dunst
+echo "    frame_color = \"$lforeground\"" | tee -a $dunst
+echo "[urgency_normal]" | tee -a $dunst
+echo "    background = \"$background\"" | tee -a $dunst
+echo "    foreground = \"$foreground\"" | tee -a $dunst
+
+rm $dunstrc
+cat $dunst | tee -a $dunstrc
+cat $dunstc | tee -a $dunstrc
+
+rm $aura
+echo $(echo $red | cut -c2-) | tee -a $aura
+
+pkill waybar
+pkill dunst
+
+waybar &
+dunst &
