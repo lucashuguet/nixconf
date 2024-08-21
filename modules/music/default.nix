468656e1f3f1a8bd8f23bc03225b11f6989d302f
AstroGoat <lucashuguet31@gmail.com>
2024-08-21 02:48:08 +0200

    enable mpd user service

--- modules/music/default.nix
+++ modules/music/default.nix
@@ -1,30 +1,25 @@
-{ pkgs, username, ... }:
+{ pkgs, config, username, ... }:
 {
   home-manager.users.${username} = {
     home.packages = with pkgs; [ mpc-cli ncmpcpp ];
-  };
+    services.mpd = {
+      enable = true;
 
-  services.mpd = {
-    enable = true;
+      musicDirectory = "/home/${username}/Music";
 
-    musicDirectory = "/home/${username}/Music";
+      playlistDirectory = "/home/${username}/.mpd/playlists";
+      dbFile = "/home/${username}/.mpd/database";
+      dataDir = "/home/${username}/.mpd/";
 
-    playlistDirectory = "/home/${username}/.mpd/playlists";
-    dbFile = "/home/${username}/.mpd/database";
-    dataDir = "/home/${username}/.mpd/";
+      network.listenAddress = "127.0.0.1";
+      network.port = 6600;
 
-    network.listenAddress = "127.0.0.1";
-    network.port = 6600;
-
-    extraConfig = ''
+      extraConfig = ''
       audio_output {
         type "pipewire"
         name "My PipeWire Output"
       }
     '';
-  };
-
-  systemd.services.mpd.environment = {
-    XDG_RUNTIME_DIR = "/run/user/1000";
+    };
   };
 }
