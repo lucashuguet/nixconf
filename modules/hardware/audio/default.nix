17d08b45f308c41fc07eeb1ee5e597e22dcdb891
AstroGoat <lucashuguet31@gmail.com>
2024-08-28 04:13:31 +0200

    add bluetooth config for audio

--- modules/hardware/audio/default.nix
+++ modules/hardware/audio/default.nix
@@ -10,11 +10,24 @@
     pulse.enable = true;
     jack.enable = true;
     wireplumber.enable = true;
+    wireplumber.extraConfig."10-bluez" = {
+      "monitor.bluez.properties" = {
+        "bluez5.enable-sbc-xq" = true;
+        "bluez5.enable-msbc" = true;
+        "bluez5.enable-hw-volume" = true;
+        "bluez5.headset-roles" = [
+          "hsp_hs"
+          "hsp_ag"
+          "hfp_hf"
+          "hfp_ag"
+        ];
+      };
+    };
   };
 
   users.users.${username} = {
     extraGroups = [ "audio" ];
   };
 
-  environment.systemPackages = with pkgs; [ pulseaudio pavucontrol ];
+  environment.systemPackages = with pkgs; [ pavucontrol ];
 }
--- modules/hardware/bluetooth/default.nix
+++ modules/hardware/bluetooth/default.nix
@@ -1,4 +1,17 @@
 {
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
+
+  hardware.bluetooth.settings = {
+    General = {
+      Name = "Computer";
+      ControllerMode = "dual";
+      FastConnectable = "true";
+      Experimental = "true";
+      Enable = "Source,Sink,Media,Socket";
+    };
+    Policy = {
+      AutoEnable = "true";
+    };
+  };
 }
