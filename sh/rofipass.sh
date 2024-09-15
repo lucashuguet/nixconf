17b586ce2f5c514f3924cdbf3142af4595d21690
AstroGoat <lucashuguet31@gmail.com>
2024-09-15 16:38:07 +0200

    don't notify if rofi isn't answered

--- sh/rofipass.sh
+++ sh/rofipass.sh
@@ -3,6 +3,11 @@
 passwords=$(find ~/.password-store/ -iname "*.gpg" | sed "s|$HOME/.password-store/||g" | sed "s/\.gpg//g")
 
 selected=$(rofi -dmenu <<< $passwords)
-echo $selected
+
+if [[ $? -eq 0 ]]; then
+  echo $selected
+else
+  exit 1
+fi
 
 notify-send "rofipass.sh" "$(pass $1 -c $selected)"
