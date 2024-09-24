d82a606c3989a04caee65fadea8533ea929d2e02
AstroGoat <lucashuguet31@gmail.com>
2024-09-24 12:24:44 +0200

    add more deps to dev shell

--- flake.nix
+++ flake.nix
@@ -61,10 +61,16 @@
         };
       };
 
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          pkg-config openssl
+      devShells.${system}.default = pkgs.mkShell rec {
+        nativeBuildInputs = with pkgs; [
+          pkg-config
         ];
+        buildInputs = with pkgs; [
+          udev openssl alsa-lib vulkan-loader
+          xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
+          libxkbcommon wayland # To use the wayland feature
+        ];
+        LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath buildInputs;
       };
     };
 }
