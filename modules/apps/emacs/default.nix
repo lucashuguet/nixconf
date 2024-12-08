9007ef1c95755a378ff14f81f2bb8a705e038278
AstroGoat <lucashuguet31@gmail.com>
2024-12-08 13:41:53 +0100

    install ripgrep as an emacs dependency

--- modules/apps/emacs/default.nix
+++ modules/apps/emacs/default.nix
@@ -1,5 +1,7 @@
 { pkgs, username, ... }:
 {
+  environment.systemPackages = with pkgs; [ ripgrep ];
+
   home-manager.users.${username} = {
     home.packages = with pkgs; [ emacs ];
     services.emacs.enable = true;
