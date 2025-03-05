e6047b581642e507ff01ca4f05c1f8f89717150b
AstroGoat <lucashuguet31@gmail.com>
2025-03-05 23:25:34 +0100

    containers related fixes

--- sh/default.nix
+++ sh/default.nix
@@ -1,6 +1,6 @@
 { pkgs, username, ... }:
 {
-  imports = [ ../modules/wm/hyprland/rofi ];
+  imports = [ ../modules/wm/hyprland/rofi ../modules/apps/browsers/surf ];
   home-manager.users.${username} = {
     home.packages =
       (with pkgs; [ fzf pywal ])
@@ -16,6 +16,7 @@
         (pkgs.writeScriptBin "rofipass" (builtins.readFile ./rofipass.sh))
         (pkgs.writeScriptBin "rofishutdown" (builtins.readFile ./rofishutdown.py))
         (pkgs.writeScriptBin "rofisink" (builtins.readFile ./rofisink.py))
+        (pkgs.writeScriptBin "rofisurf" (builtins.readFile ./rofisurf.py))
         (pkgs.writeScriptBin "rofitheme" (builtins.readFile ./rofitheme.sh))
         (pkgs.writeScriptBin "aurastatic" ''
           asusctl led-mode static -c $(cat ~/.config/colors/aura.txt)
