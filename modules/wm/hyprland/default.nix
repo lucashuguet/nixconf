25f6791ce4616ae26ab503815e194ca5190e9afb
AstroGoat <lucashuguet31@gmail.com>
2025-11-04 11:08:24 +0100

    change brightness without sudo

--- modules/wm/hyprland/default.nix
+++ modules/wm/hyprland/default.nix
@@ -13,6 +13,8 @@
   };
 
   programs.light.enable = true;
+  users.users.${username}.extraGroups = [ "video" ];
+
   home-manager.users.${username} = {
     home.file = {
       ".config/hypr/hyprland.conf".source = ./hyprland.conf;
