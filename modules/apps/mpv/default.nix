b552e1329482ceb94697d11c04b50b8d2a3b746a
AstroGoat <lucashuguet31@gmail.com>
2025-12-21 01:58:33 +0100

    add cdda support to mpv

--- modules/apps/mpv/default.nix
+++ modules/apps/mpv/default.nix
@@ -1,7 +1,13 @@
 { pkgs, username, ... }:
 {
   home-manager.users.${username} = {
-    home.packages = with pkgs; [ mpv ];
+    home.packages = with pkgs; [
+      (mpv.override {
+        mpv = mpv-unwrapped.override {
+          cddaSupport = true;
+        };
+      })
+    ];
     home.file = {
       ".config/mpv" = {
         source = ./config;
