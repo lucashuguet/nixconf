d9f218f6a7ca8b8f039ff99c3e767079b6e9a4a2
AstroGoat <lucashuguet31@gmail.com>
2025-12-22 19:05:43 +0100

    add brasero

--- modules/apps/optical/default.nix
+++ modules/apps/optical/default.nix
@@ -1,10 +1,10 @@
 { pkgs, username, ... }:
 {
   environment.systemPackages = with pkgs; [
+    brasero
     dvdbackup
     handbrake
     kdePackages.k3b
-
   ];
 
   users.users.${username}.extraGroups = [ "cdrom" ];
