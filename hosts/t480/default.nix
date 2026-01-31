b06b8b1f2de1dc6dcb03affc2700a52ddc1f069e
Lucas Huguet <lucashuguet31@gmail.com>
2026-01-31 15:55:11 +0100

    setup t480 fingerprint reader

--- hosts/t480/default.nix
+++ hosts/t480/default.nix
@@ -1,16 +1,30 @@
 # Lenovo Thinkpad T480
 
-{ nixos-hardware, username, ... }:
+{
+  nixos-06cb-009a-fingerprint-sensor,
+  nixos-hardware,
+  username,
+  ... }:
 {
   imports = [
     ./hardware-configuration.nix ./disko-config.nix
     ../../modules/hardware/nvidia
+    nixos-06cb-009a-fingerprint-sensor.nixosModules."06cb-009a-fingerprint-sensor"
     nixos-hardware.nixosModules.lenovo-thinkpad-t480
   ];
 
   system.stateVersion = "25.11";
 
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.extraModprobeConfig = ''
+    options psmouse synaptics_intertouch=1
+  '';
+
+  services."06cb-009a-fingerprint-sensor" = {
+    enable = true;
+    backend = "libfprint-tod";
+    calib-data-file = ./calib-data.bin;
+  };
 
   sops.secrets = {
     "hosts/t480/komga_api".owner = username;
