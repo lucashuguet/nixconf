a1b6b463222ca8f11a7c247d4aba7b0ed0bd415a
Lucas Huguet <lucashuguet31@gmail.com>
2026-02-11 02:00:15 +0100

    hide regreet window decoration

--- modules/dm/greetd/default.nix
+++ modules/dm/greetd/default.nix
@@ -1,8 +1,9 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   programs.regreet = {
     enable = true;
     cursorTheme.name = config.theme.cursor.name;
+    cageArgs  = [ "-s" "-d" ];
     font.name = config.theme.font.mono.name;
     iconTheme.name = config.theme.icon.name;
     theme.name = config.theme.gtk.name;
