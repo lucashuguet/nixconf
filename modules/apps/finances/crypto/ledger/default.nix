05fd5f3802cc18f78f9bc151e74c07855a173210
Lucas Huguet <lucashuguet31@gmail.com>
2026-02-16 00:22:09 +0100

    replace ugly udev rules by nixos option

--- modules/apps/finances/crypto/ledger/default.nix
+++ modules/apps/finances/crypto/ledger/default.nix
@@ -4,14 +4,5 @@
     home.packages = with pkgs; [ unstable.ledger-live-desktop ];
   };
 
-  # fix read+write permissions for ledger
-  services.udev.extraRules = ''
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", TAG+="uaccess", TAG+="udev-acl"
-    KERNEL=="hidraw*", ATTRS{idVendor}=="2c97", MODE="0666"
-  '';
-
-  # KERNEL=="hidraw*", ATTRS{idVendor}=="2c97", MODE="0660", GROUP="uucp"
-  users.users.${username} = {
-    extraGroups = [ "uucp" ];
-  };
+  hardware.ledger.enable = true;
 }
