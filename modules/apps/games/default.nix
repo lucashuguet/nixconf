83d1759954484e5f8dada459cde603b4a8eb9b35
Lucas Huguet <lucashuguet31@gmail.com>
2026-02-16 00:21:28 +0100

    gaming improvements

--- modules/apps/games/default.nix
+++ modules/apps/games/default.nix
@@ -1,5 +1,11 @@
+{ pkgs, username, ... }:
 {
   imports = [ ./lutris ];
 
+  environment.systemPackages = with pkgs; [ gamemode ];
+
   hardware.xone.enable = true;
+  users.users.${username} = {
+    extraGroups = [ "input" ];
+  };
 }
