c6c58eb19084842a282502971352ea4ff6d4c055
Lucas Huguet <lucashuguet31@gmail.com>
2026-05-13 08:49:16 +0200

    install emacs markdown-mode

--- pkgs/emacs/default.nix
+++ pkgs/emacs/default.nix
@@ -17,7 +17,7 @@
     # major modes
     org-superstar evil-org org-roam org-roam-ui
     rust-mode nix-ts-mode web-mode emmet-mode beancount
-    typst-ts-mode
+    typst-ts-mode markdown-mode
     (treesit-grammars.with-grammars (g: with g; [
       tree-sitter-bash
       tree-sitter-c
