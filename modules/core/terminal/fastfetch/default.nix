0c1a31e7bf6ab153fc811390673bcfe25757aeec
Lucas Huguet <lucashuguet31@gmail.com>
2026-06-15 08:50:38 +0200

    customize fastfetch

--- modules/core/terminal/fastfetch/default.nix
+++ modules/core/terminal/fastfetch/default.nix
@@ -4,7 +4,29 @@
 
     home-manager.users.${username} = {
       home.file = {
-        ".config/fastfetch/config.jsonc".source = ./config.jsonc;
+        ".config/fastfetch/config.jsonc".text = ''
+          {
+            "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
+            "logo": {
+              "source": "${./logo.txt}",
+            },
+            "modules": [
+              "title",
+              "separator",
+              "os",
+              "host",
+              "packages",
+              "de",
+              "wm",
+              "terminal",
+              "cpu",
+              "gpu",
+              "memory",
+              "break",
+              "colors"
+            ]
+          }
+        '';
       };
     };
   };
